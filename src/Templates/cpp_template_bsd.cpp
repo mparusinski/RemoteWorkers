d22081004bdce6458d100aa91ec63ffce007365a
Michal Parusinski <mparusinski@googlemail.com>
2012-05-17 12:51:17 +0100

    Changing license form GPLv2 to 3-clause (new) BSD License

---
+++ src/Templates/cpp_template_bsd.cpp
@@ -0,0 +1,11 @@
+/* 
+
+<file name>: <purpose>
+
+As part of the Remote Workers Project which creates a framework for remote
+management of laptops, desktop and servers. 
+
+Copyright (C) 2012 Remote Workers Project. All rights reserved.
+Created by <author> on <date>
+
+*/
