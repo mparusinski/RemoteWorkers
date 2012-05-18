64ffd4dfc151ce7c398d56fba54b3929bad671eb
Michal Parusinski <mparusinski@googlemail.com>
2012-05-18 17:22:12 +0100

    Added first test case which checks basic functionality

---
+++ src/ControlUnit/Tests/SimpleRun/SimpleRun.cpp
@@ -0,0 +1,71 @@
+/* 
+
+SimpleRun.cpp: Test to check that basic functionality works
+
+As part of the Remote Workers project which creates a framework for remote
+management of laptops, desktop and servers. 
+
+Copyright (C) 2012 Remote Workers Project. All rights reserved.
+Created by Michal Parusinski <mparusinski@googlemail.com> on 15/05/2012.
+
+*/
+
+// NO FUNCTIONS TO BE ADDED TO THIS FILE
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "WorkerInterface/Reply.hpp"
+#include "WorkerInterface/Worker.hpp"
+#include "WorkerInterface/Command.hpp"
+#include "WorkerInterface/Management.hpp"
+#include "Utils/Logger.hpp"
+
+using namespace std;
+using namespace WorkerInterface;
+
+int main(int argc, char *argv[])
+{
+	Utils::Logger::getInstance()->turnAllOn();
+
+	vector<string> availableWorkers = Management::getInstance()->listAvailableWorkers();
+	for (size_t i = 0; i < availableWorkers.size(); ++i)
+	{
+		cout << availableWorkers[i] << endl;
+	}
+
+	Worker worker = Management::getInstance()->createWorker("SimpleTestWorker");
+	Command command("SimpleTestWorker", vector<string>());
+	worker.executeCommand(command);
+	Reply reply = worker.getReply();
+
+	if ( reply.empty() )
+	{
+		Utils::Logger::getInstance()->error("Reply is not built");
+		return -1;
+	}
+	else
+	{
+		Utils::Logger::getInstance()->log("Reply is built");
+	}
+
+	vector< pair< string, ByteStream > > rawData = reply.getRawData();
+	const size_t numberOfByteStreams = rawData.size();
+	for (size_t i = 0; i < numberOfByteStreams; ++i)
+	{
+		const string& fileName = rawData[i].first;
+		ByteStream& byteStream = rawData[i].second;
+		cout << "File: " << fileName << endl;
+
+		const size_t lengthOfStream = byteStream.size();
+		const char * rawData = byteStream.getRawData();
+		for (size_t j = 0; j < lengthOfStream; ++j)
+		{
+			cout << rawData[j];
+		}
+		cout << endl << endl;
+	}
+
+	return 1;
+}
