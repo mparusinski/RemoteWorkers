7e5908813c8bf3a6976e80253ec3657f0c7c637e
Michal Parusinski <mparusinski@googlemail.com>
2012-06-20 17:12:57 +0200

    Added programming folder

---
+++ src/ControlUnit/Utils/Macros/Macros.h
@@ -0,0 +1,16 @@
+/* 
+
+<file name>: <purpose>
+
+As part of the RemoteWorkers program which creates a framework for remote
+management of laptops, desktop and servers. 
+
+Copyright (C) 2012 Remote Workers Project. All rights reserved.
+Created by <author> on <date>.
+
+*/
+
+#ifndef _FILE_
+#define _FILE_
+
+#endif // _FILE_
--- src/ControlUnit/Utils/Return/Return.cc
+++ src/ControlUnit/Utils/Return/Return.cc
@@ -1,13 +1,14 @@
 /* 
+ 
+ Return.cpp: Return error message
+ 
+ As part of the RemoteWorkers program which creates a framework for remote
+ management of laptops, desktop and servers. 
+ 
+ Copyright (C) 2012 Remote Workers Project. All rights reserved.
+ Created by Michal Parusinski <mparusinski@googlemail.com> on 15/06/2012.
+ 
+ */
 
-Return.cpp: Return error message
+#include "Return.h"
 
-As part of the Remote Workers Project which creates a framework for remote
-management of laptops, desktop and servers. 
-
-Copyright (C) 2012 Remote Workers Project. All rights reserved.
-Created by Michal Parusinski <mparusinski@googlemail.com> on 15/06/2012.
-
-*/
-
-#include "Return.hpp"
