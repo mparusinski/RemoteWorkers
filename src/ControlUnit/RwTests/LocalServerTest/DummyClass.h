252a229f34f0330069f80c3b0a4d9b733ef9aae8
Michal Parusinski <mparusinski@googlemail.com>
2012-06-26 21:12:52 +0100

    LocalTestServer is almost ready for use. It will be use for extensive testing of the framework

--- src/ControlUnit/RwTests/LocalServerTest/DummyClass.h
+++ src/ControlUnit/RwTests/LocalServerTest/DummyClass.h
@@ -13,8 +13,14 @@ Created by Michal Parusinski <mparusinski@googlemail.com> on 22/06/2012.
 #ifndef _RWTESTS_LOCALSERVERTEST_DUMMYCLASS_H_
 #define _RWTESTS_LOCALSERVERTEST_DUMMYCLASS_H_
 
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QString>
+#include <QLabel>
 #include <QPushButton>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QTextEdit>
 #include <QThread>
 
 #include "RwNetworking/RwServers/RwCommandServerLocal.h"
@@ -55,29 +61,66 @@ public:
     explicit DummyClass(QWidget* parent = 0) : QWidget(parent) 
     { 
         m_localClient = new RwClients::RwCommandClientLocal(this, SERVER_NAME);
+        
+        m_currentReply = new RwNetDataStructures::RwCommandReply;
+        
+        m_mainLayout = new QVBoxLayout(this);
+        
+        m_workerNameDialog = new QHBoxLayout(this);
+        m_workerNameLabel  = new QLabel(this);
+        m_workerNameEdit   = new QLineEdit(this);
+        
+        m_orderNameDialog  = new QHBoxLayout(this);
+        m_orderNameLabel   = new QLabel(this);
+        m_orderNameEdit    = new QLineEdit(this);
+        
+        m_replyDialog      = new QHBoxLayout(this);
+        m_replyFileList    = new QListWidget(this);
+        m_replyTextView    = new QTextEdit(this);
+        
         m_button = new QPushButton(this);
-        m_button->setText("Push button to connect");
+        
+        m_workerNameDialog->addWidget(m_workerNameLabel);
+        m_workerNameDialog->addWidget(m_workerNameEdit);
+        
+        m_orderNameDialog->addWidget(m_orderNameLabel);
+        m_orderNameDialog->addWidget(m_orderNameEdit);
+        
+        m_replyDialog->addWidget(m_replyFileList);
+        m_replyDialog->addWidget(m_replyTextView);
+        
+        m_mainLayout->addLayout(m_workerNameDialog);
+        m_mainLayout->addLayout(m_orderNameDialog);
+        m_mainLayout->addWidget(m_button);
+        m_mainLayout->addLayout(m_replyDialog);
+        
+        m_workerNameLabel->setText("Worker name");
+        m_orderNameLabel->setText("Order name");
+        
+        m_button->setText("Send request");
         setWindowTitle("Dummy window");
+        
+        m_replyTextView->setReadOnly(true);
         
         QObject::connect(m_button, SIGNAL(pressed()), this, SLOT(run()));
         QObject::connect(m_localClient, SIGNAL(replyReady()), this, SLOT(obtainedReply()));
+        QObject::connect(m_replyFileList, SIGNAL(clicked(QModelIndex)), this, SLOT(changeReply(QModelIndex)));
     }
     
-    ~DummyClass()
+    virtual ~DummyClass()
     {
-        delete m_button;
+        delete m_currentReply;
+        // Should be handled by QT
     }
     
     public slots:
     
     void run()
     {
-        rwMessage() << "Button pressed" << endLine();
-        
         m_localClient->connectToServer();
         
-        QString workerName = "SimpleTestWorker";
-        QString order      = "SimpleTestWorker";
+        QString workerName = m_workerNameEdit->text();
+        QString order      = m_orderNameEdit->text();
         QStringList arguments;
         RwNetDataStructures::RwCommandRequest request(workerName, RwWorkerInterface::RwCommand(order, arguments));
         m_localClient->sendRequest(request);
@@ -85,27 +128,56 @@ public:
     
     void obtainedReply()
     {
-        RwNetDataStructures::RwCommandReply reply;
-        m_localClient->getReply(reply);
+        m_localClient->getReply(*m_currentReply);
         
-        if (reply.isError()) {
+        m_replyFileList->clear();
+        m_replyTextView->clear();
+        
+        if (m_currentReply->isError()) {
             rwError() << "An error has occurred when processing request" << endLine();
+            m_replyTextView->setText("An error has occurred when processing request");
         } else {
-            const RwWorkerInterface::RwReply& actualReply =  reply.getReply();
+            const RwWorkerInterface::RwReply& actualReply =  m_currentReply->getReply();
             const RwWorkerInterface::RwReply::ByteArrays& arrays = actualReply.getRawData();
             const int numberOfReplies = arrays.length();
             for (int i = 0; i < numberOfReplies; ++i)
             {
-                const QByteArray& rawData = arrays[i].second;
-                const char* cString = rawData.data();
-                rwMessage() << "File: " << arrays[i].first << endLine();
-                rwMessage() << cString << endLine();
+                m_replyFileList->addItem(arrays[i].first);
             }
         }
     }
     
+    void changeReply(const QModelIndex& index)
+    {
+        const int i = index.row();
+        
+        const RwWorkerInterface::RwReply& actualReply =  m_currentReply->getReply();
+        const RwWorkerInterface::RwReply::ByteArrays& arrays = actualReply.getRawData();
+        
+        const QByteArray& rawData = arrays[i].second;
+        const QString& text(rawData.data());
+        
+        m_replyTextView->setText(text);
+    }
+    
 private:
     RwClients::RwCommandClientLocal * m_localClient;
+    QVBoxLayout* m_mainLayout;
+    
+    QHBoxLayout* m_workerNameDialog;
+    QLabel* m_workerNameLabel;
+    QLineEdit* m_workerNameEdit;
+    
+    QHBoxLayout* m_orderNameDialog;
+    QLabel* m_orderNameLabel;
+    QLineEdit* m_orderNameEdit;
+    
+    QHBoxLayout *m_replyDialog;
+    QListWidget *m_replyFileList;
+    QTextEdit *m_replyTextView;
+    
+    RwNetDataStructures::RwCommandReply* m_currentReply;
+    
     QPushButton* m_button;
     
 };
